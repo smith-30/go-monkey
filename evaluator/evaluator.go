2796871cabeb929b2434f4ec6c13125a9273a340
misu <parkour12019@gmail.com>
2018-07-08 10:11:22 +0900

    upgrade: bool

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"github.com/smith-30/go-monkey/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// statement
@@ -16,7 +21,7 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return nativeBooleanObject(node.Value)
 	}
 	return nil
 }
@@ -29,4 +34,12 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBooleanObject(i bool) *object.Boolean {
+	if i {
+		return TRUE
+	}
+
+	return FALSE
 }
