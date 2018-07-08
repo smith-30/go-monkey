dca7aee89c9ddb23e8d557e69770adc857221a8c
misu <parkour12019@gmail.com>
2018-07-08 10:06:31 +0900

    add: boolean

--- ast/program.go
+++ ast/program.go
@@ -9,6 +9,7 @@ type (
 	}
 )
 
+// Todo refactoring
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -26,14 +26,6 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
-func testEval(i string) object.Object {
-	l := lexer.New(i)
-	p := parser.New(l)
-	program := p.ParseProgram()
-
-	return Eval(program)
-}
-
 func testIntegerObject(t *testing.T, obj object.Object, exp int64) bool {
 	result, ok := obj.(*object.Integer)
 	if !ok {
@@ -45,4 +37,43 @@ func testIntegerObject(t *testing.T, obj object.Object, exp int64) bool {
 		return false
 	}
 	return true
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		exp   bool
+	}{
+		{"1", "false", false},
+		{"2", "true", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			evaluated := testEval(tt.input)
+			testBooleanObject(t, evaluated, tt.exp)
+		})
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, exp bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != exp {
+		t.Errorf("object has wrong value. got=%t, want=%t", result.Value, exp)
+		return false
+	}
+	return true
+}
+
+func testEval(i string) object.Object {
+	l := lexer.New(i)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
 }
