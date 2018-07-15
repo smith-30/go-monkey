933954816556a375f9502b8c889262d9794e1e7e
misu <parkour12019@gmail.com>
2018-07-15 17:15:19 +0900

    add: sample

--- main.go
+++ main.go
@@ -18,3 +18,31 @@ func main() {
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
+
+// let map = fn(arr, f) {
+// 	let iter = fn(arr, accumlated) {
+// 		if (len(arr) == 0) {
+// 			accumlated
+// 		} else {
+// 			iter(rest(arr), push(accumlated, f(first(arr))))
+// 		}
+// 	};
+
+// 	iter(arr, []);
+// }
+
+// let reduce = fn(arr, initial, f) {
+// 	let iter = fn(arr, result) {
+// 		if (len(arr) == 0) {
+// 			result
+// 		} else {
+// 			iter(rest(arr), f(result, first(arr)))
+// 		}
+// 	};
+
+// 	iter(arr, initial);
+// }
+
+// let sum = fn(arr) {
+// 	reduce(arr, 0, fn(initial, el) {initial + el});
+// };
