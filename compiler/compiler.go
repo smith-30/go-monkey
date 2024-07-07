519b8c07625102af4b3fab165e83979d57d6a488
smith-30 <smith-30@example.com>
2024-07-07 22:58:17 +0900

    mod: compiler

---
+++ compiler/compiler.go
@@ -0,0 +1,33 @@
+package compiler
+
+import (
+	"github.com/smith-30/go-monkey/ast"
+	"github.com/smith-30/go-monkey/code"
+	"github.com/smith-30/go-monkey/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
