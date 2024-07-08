868e7b904ebb12cee8a55698825a49f32c1cbcaf
smith-30 <smith-30@example.com>
2024-07-08 22:18:03 +0900

    mod: parse integer

--- code/code_test.go
+++ code/code_test.go
@@ -42,6 +42,7 @@ func TestInstructionsString(t *testing.T) {
 		Make(OpConstant, 2),
 		Make(OpConstant, 65535),
 	}
+
 	expected := `0000 OpConstant 1
 0003 OpConstant 2
 0006 OpConstant 65535
--- compiler/compiler.go
+++ compiler/compiler.go
@@ -18,8 +18,56 @@ func New() *Compiler {
 	}
 }
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, item := range node.Statements {
+			err := c.Compile(item)
+			if err != nil {
+				return err
+			}
+		}
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
+
 	return nil
 }
+
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+// emit はコンパイラ用語で、"generate"（生成する）と "output"（出力する）を意味する
+// 命令を生成し、それを印刷したり、ファイルに書き込んだり、
+// メモリ上のコレクションに追加したりして、結果に追加する
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...)
+	pos := c.addInstruction(ins)
+	return pos
+}
+
+func (c *Compiler) addInstruction(ins []byte) int {
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction
+}
+
 func (c *Compiler) Bytecode() *Bytecode {
 	return &Bytecode{
 		Instructions: c.instructions,
