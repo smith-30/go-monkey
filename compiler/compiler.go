751098aa00c00c03d4ece4a6a7ad952a61f25a6e
smith-30 <smith-30@example.com>
2024-08-17 12:22:25 +0900

    mod: index operator

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -41,6 +41,16 @@ func (c *Compiler) Compile(node ast.Node) error {
 				return err
 			}
 		}
+	case *ast.IndexExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+		err = c.Compile(node.Index)
+		if err != nil {
+			return err
+		}
+		c.emit(code.OpIndex)
 	case *ast.PrefixExpression:
 		err := c.Compile(node.Right)
 		if err != nil {
